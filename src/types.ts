e945f889cc8c2fc80b55ffb62029368f5f628d86
Arjun Vikram <arjvik@gmail.com>
2023-01-31 06:04:28 -0800

    Revert "Fix bug serializing bigint using canonicalize"
    This reverts commit 3c676f586f748f371d598f1b73abd1c5fd79b1b2.

--- src/types.ts
+++ src/types.ts
@@ -51,7 +51,7 @@ const Outpoint = z.object({
     index: z.number().int().nonnegative()
 })
 
-export type UTXO = z.infer<typeof Outpoint> & { value: number }
+export type UTXO = z.infer<typeof Outpoint> & { value: bigint }
 
 const TransactionCommon = z.object({
     type: z.literal('transaction')
@@ -61,7 +61,7 @@ export const TransactionObject = z.union([
         z.object({
             outputs: z.array(z.object({
                 pubkey: Hash,
-                value: z.number().int().nonnegative()
+                value: z.number().int().nonnegative().transform(BigInt)
             })),
             inputs: z.array(z.object({
                 outpoint: Outpoint,
@@ -72,7 +72,7 @@ export const TransactionObject = z.union([
         z.object({
             outputs: z.array(z.object({
                 pubkey: Hash,
-                value: z.number().int().nonnegative()
+                value: z.number().int().nonnegative().transform(BigInt)
             })).length(1),
             height: z.number().int().nonnegative()
         }).strict())
