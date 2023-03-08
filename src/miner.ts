00e5a2e91968b0ac2968284837c3be7cb764aac3
Arjun Vikram <arjvik@gmail.com>
2023-03-07 18:40:31 -0800

    Fix off-by-one error

--- src/miner.ts
+++ src/miner.ts
@@ -59,7 +59,7 @@ class Miner {
       "created": Math.floor(new Date().getTime() / 1000),
       "miner": NAME,
       "nonce":"<<NONCE>>",
-      "note":`Block Height: ${this.height}`,
+      "note":`Block Height: ${this.height+1}`,
       "previd": this.prevblock,
       "studentids":["arjvik","aalinur"],
       "txids":[this.coinbase??'', ...txs],
